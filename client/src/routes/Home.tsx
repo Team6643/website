a7aa31ce97fdb0f23a42728d42649aabcb514837
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2025-01-27 21:56:25 -0600

    fix: more responsiveness

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,11 +1,13 @@
 import Button from "../components/Button";
 import baxterLogo from "../assets/baxter.svg";
 import dpiLogo from "../assets/dpi.svg";
+import { useNavigate } from "@solidjs/router";
 
 const Home = () => {
+    const navigate = useNavigate();
     return (
         <>
-            <div class="grid grid-cols-2 justify-start items-start flex-grow-0 flex-shrink-0 relative gap-[90px] my-32 px-10">
+            <div class="grid grid-cols-2 justify-start items-start flex-grow-0 flex-shrink-0 relative gap-[90px] my-32 px-10 max-w-[1500px]">
                 <div class="flex-grow-0 flex-shrink-0 max-w-[560px] h-[350px] bg-[#d9d9d9] rounded-xl col-span-2 lg:col-span-1" />
                 <div class="flex flex-col justify-start items-start flex-grow overflow-hidden gap-[50px] col-span-2 lg:col-span-1">
                     <div class="flex flex-col justify-start items-start self-stretch flex-grow-0 flex-shrink-0 relative gap-2">
@@ -20,7 +22,7 @@ const Home = () => {
                     <Button>Learn more</Button>
                 </div>
             </div>
-            <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 bg-[#d4c6ff]">
+            <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 gap-2.5 py-8 bg-[#d4c6ff] px-5">
                 <div class="flex flex-col justify-start items-center flex-grow-0 flex-shrink-0 relative gap-10">
                     <p class="self-stretch flex-grow-0 flex-shrink-0 text-4xl font-bold text-center text-black">
                         Top sponsors
@@ -30,16 +32,16 @@ const Home = () => {
                             
                         <a href="https://www.baxter.com/" class="col-span-2 lg:col-span-1 flex justify-center"><img src={baxterLogo} alt="baxter logo" class="w-[418px] h-[72px]" /></a>
                     </div>
-                    <Button>See more</Button>
+                    <Button onClick={() => navigate("/sponsors")}>See more</Button>
                 </div>
             </div>
-            <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-5 my-32">
+            <div class="flex flex-col justify-start items-center self-stretch flex-grow-0 flex-shrink-0 relative gap-5 my-32 px-5">
                 <p class="flex-grow-0 flex-shrink-0 text-4xl font-bold text-left text-black">About us</p>
                 <p class="flex-grow-0 flex-shrink-0 max-w-[600px] text-xl text-center text-black">
                     We are a student-led organization working to drive positive change in the world by introducing
                     opportunities to youth to learn about the possibilities made available through STEM.
                 </p>
-                <Button>See more</Button>
+                <Button onClick={() => navigate("/about")}>See more</Button>
             </div>
         </>
     );
